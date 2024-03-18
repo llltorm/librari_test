8bef780d0a0d9d71282ae1f09ee299b3bb9ef97c
llltorm <86806500+llltorm@users.noreply.github.com>
2024-03-18 20:28:22 +0300

    1
    1

---
+++ library/src/main/java/net/project/library/UserRepository.java
@@ -0,0 +1,10 @@
+package net.project.library;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    User findByUsername(String username);
+}
